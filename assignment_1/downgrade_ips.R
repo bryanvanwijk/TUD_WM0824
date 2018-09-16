a242b839246112a87c4ec6796b667109630b892f
Andrew Jiang <dev@andrewjiang.me>
2018-09-16 20:26:42 +0200

    A1: Clean up downgrade_ips.R
    Get rid of unused commented code, add comments to describe the code.

--- assignment_1/downgrade_ips.R
+++ assignment_1/downgrade_ips.R
@@ -1,3 +1,7 @@
+# This script is used to determine the number of servers that are using the insecure SSLv3 handshake protocol at the end
+# of the data set provided, grouped by country. It is also used to determine the servers that was previously on a secure
+# handshake protocol, but has since reverted to using SSLv3.
+
 Sys.setenv('R_MAX_VSIZE'=64000000000)
 library(dplyr)
 # Load data set
@@ -14,40 +18,22 @@ rm(mydata)
 # Add a POSIX timestamp column to make date comparisons simpler
 df$posix_timestamp <- as.POSIXct(df$timestamp, format="%Y-%m-%d %H:%M:%OS")
 
-#ips <- data.frame(table(df$ip))
-#handshakes <- data.frame(table(df$handshake))
+# We define a server as a (IP, port) pair, since one IP could be the host of multiple servers on different ports.
+servers <- df %>% arrange(ip, port, posix_timestamp) %>% group_by(ip, port)
 
-# Count of number of handshakes per protocol
-handshakes <- df %>% group_by(df$handshake) %>% summarise(count = n())
-
-ips <- df %>% arrange(ip, port, posix_timestamp) %>% group_by(ip, port)
-
-#ips_that_changed_back <- ips %>% filter(handshake == "SSLv3" & lag(handshake) != "SSLv3")
-
-last_entry_per_server <- ips %>% filter(posix_timestamp == max(posix_timestamp))
+# Generate a list of the very last entry we have for each server in the dataset.
+last_entry_per_server <- servers %>% filter(posix_timestamp == max(posix_timestamp))
 
 currently_insecure_servers <- last_entry_per_server[last_entry_per_server$handshake == "SSLv3",]
 currently_secure_servers <- last_entry_per_server[last_entry_per_server$handshake != "SSLv3",]
 insecure_servers_by_country <- currently_insecure_servers %>% group_by(subject_country) %>% summarise(count = n())
-#insecure_servers_no_country <- currently_insecure_servers[currently_insecure_servers$subject_country == "",]
 
-
-#ddply(df, .(posix_timestamp), summarize, percentage = sum(handshake != "SSLv3"))
-
-#View(ips)
-#mydata[mydata$ip == "104.103.178.77",]
-#ip_104_103_178_77 <- mydata[mydata$ip == "104.103.178.77",]
-#View(ip_104_103_178_77)
-#ip_13_95_150_238 <- mydata[mydata$ip == "13.95.150.238",]
-#View(ip_13_95_150_238)
-
-#hosts <- subset(df, handshake == 'SSLv3')
-#ipAddresses <- unique(hosts$ip)
-  
-#last_entry_per_ip_mult <- df %>% group_by(ip) %>% filter(n() > 1) %>% filter(posix_timestamp == max(posix_timestamp))
-
-ips_with_mult_entries <- ips %>% filter(n() > 1)
-servers_that_used_secure_comm <- ips_with_mult_entries[ips_with_mult_entries$handshake != "SSLv3",]
+# We only wish to look at servers that actually have multiple entries associated with them, since
+# a server can't go from using something secure to SSLv3 if there's only ever one entry for it.
+servers_with_mult_entries <- servers %>% filter(n() > 1)
+servers_that_used_secure_comm <- servers_with_mult_entries[servers_with_mult_entries$handshake != "SSLv3",]
 servers_that_used_secure_comm <- servers_that_used_secure_comm %>% select(ip, port)
 servers_that_used_secure_comm <- unique(servers_that_used_secure_comm)
-ips_that_reverted_back_to_sslv3 <- inner_join(currently_insecure_servers, servers_that_used_secure_comm, by=c("ip", "port"))
+
+# Determine the servers that reverted back to using SSLv3 after using a more secure handshake protocol
+servers_that_reverted_back_to_sslv3 <- inner_join(currently_insecure_servers, servers_that_used_secure_comm, by=c("ip", "port"))
